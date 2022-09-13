0d662c885dbaec50d1ba108ae4c9c9041657f389
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-13 11:50:42 +0300

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/hello', function () {
+    return response('<h1>Hello world</h1>', 200)
+    ->header('Content-Type', 'text/plain')
+    ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function ($id) {
+    ddd($id);
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function (Request $request) {
+    return $request->name . ' ' . $request->city;
+});
