d066296bcd0cd281818c7f1caea7962a029ae77d
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-14 11:51:22 +0300

    layout logo not fixed

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -10,8 +10,8 @@
     <div class="flex">
         <img
         class="hidden w-48 mr-6 md:block" 
-        src="" 
-        alt=""
+        src="{{asset('images/default.png')}}" 
+        alt="logo"
         />
     <div>
     <h3 class="text-2xl">
