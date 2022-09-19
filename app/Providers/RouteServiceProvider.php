d24b951def04012895496b4510c9f10c71e33bb1
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-19 10:10:40 +0300

    route protect

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = 'http://localhost/delfinai/laravel-project1/public/listings';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
